61d4d0e1bff5e0bceb0ecbad704930e9623fd53e
andreandre2245 <70665449+andreandre2245@users.noreply.github.com>
2024-04-08 21:52:42 -0500

    Update testspec.js

--- spec/testspec.js
+++ spec/testspec.js
@@ -1,5 +1,7 @@
 const { error } = require("console");
+const { Hash } = require("crypto");
 const { response } = require("express");
+const { url } = require("inspector");
 var request = require("request");
 
 var base_url = "http://localhost:3000/"
@@ -8,6 +10,7 @@ describe("SWE Project", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBeDefined();
         expect(response.statusCode).toBe(200);
         done();
       });
@@ -27,7 +30,30 @@ describe("SWE Project", function() {
       });
     });
 
+    it("Error response checking", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(error).toBeDefined();
+        done();
+      });
+    });
+
+    it("deployment URL active", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(base_url).toBeDefined();
+        done();
+      });
+    });
+
+    it("complete URL", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(base_url.substring).toBeDefined();
+        expect(base_url.length).toBeDefined();
+        expect(base_url.length).toBe(22);
+        done();
+      });
+    });    
 
 
   });
 });
+
